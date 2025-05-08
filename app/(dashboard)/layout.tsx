dd12db3b990f40ec7eeb79192665ef9a07f0531e
auziqni <65333301+auziqni@users.noreply.github.com>
2025-05-09 05:53:58 +0700

    feat: sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,21 +1,21 @@
-// "use client";
-// import { useState } from "react";
+"use client";
+import { useState } from "react";
 import Navbar from "@/components/dashboard/navbar";
 import Sidebar from "@/components/dashboard/sidebar";
 import { cn } from "@/lib/utils";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   // sidebar state
-  // const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <main className="relative grid w-screen grid-cols-12">
-      {/* <Sidebar
+      <Sidebar
         className={cn(isOpen ? "flex" : "hidden", " lg:flex col-span-2")}
         isOpen={isOpen}
         setIsOpen={setIsOpen}
-      /> */}
+      />
       <div className="relative flex flex-col w-full col-span-12 lg:col-span-10">
-        {/* <Navbar isOpen={isOpen} setIsOpen={setIsOpen} /> */}
+        <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
         {children}
       </div>
     </main>

2c391f97c2fc80196a90f49970a60de2fa9c6f38
auziqni <65333301+auziqni@users.noreply.github.com>
2025-05-15 04:56:50 +0700

    feat: get mahasiswa

---
+++ app/api/getmahasiswa/route.ts
@@ -0,0 +1,26 @@
+//get mahasiswa
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@/lib/generated/prisma";
+
+const prisma = new PrismaClient();
+export async function GET() {
+  try {
+    const mahasiswa = await prisma.mahasiswa.findMany({
+      select: {
+        id: true,
+        nama: true,
+        email: true,
+        password: true,
+      },
+      orderBy: {
+        nama: "asc",
+      },
+    });
+    return NextResponse.json(mahasiswa);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch data" },
+      { status: 500 }
+    );
+  }
+}
